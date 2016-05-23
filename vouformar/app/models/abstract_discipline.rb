60311dc3aa8932ab917ef3f8d6f62e9a233534b1
EduardoBrasil <brasil.eduardo1@gmail.com>
2016-05-23 01:22:25 -0300

    validate attributes in abstract_course and abstract_discipline

--- vouformar/app/models/abstract_discipline.rb
+++ vouformar/app/models/abstract_discipline.rb
@@ -1,2 +1,9 @@
 class AbstractDiscipline < ActiveRecord::Base
+  validates_presence_of :codeDiscipline, message: "Disciplina é obrigatório"
+  validates_numericality_of :codeDiscipline, message: "Somente numeros"
+	validates_presence_of :nameDiscipline, message: "Nome da disciplina é obrigatório"
+	validates_presence_of :description, message: "A descricao não pode ser nula"
+	validates_length_of :credits, :maximum => 2, message: "Máximo 2 digitos"
+  validates_numericality_of :credits, message: "Somente numeros"
+  validates_presence_of :credits, message: "Quantidade de Creditos é um campo obrigatorio"
 end
