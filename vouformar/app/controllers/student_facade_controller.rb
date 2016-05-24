dbd19f638c3dea1739b367d781b8daf4960b48e3
Vitor Nere <vitor.nere@hotmail.com>
2016-05-24 14:34:51 -0300

    Gerado classes para authenticate e facade

---
+++ vouformar/app/controllers/student_facade_controller.rb
@@ -0,0 +1,2 @@
+class StudentFacadeController < ApplicationController
+end
---
+++ vouformar/test/controllers/student_facade_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StudentFacadeControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
