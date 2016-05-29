e537f57bc93c0ed86f14daad21faaef2918fd4b1
Vitor Nere <vitor.nere@hotmail.com>
2016-05-29 19:13:03 -0300

    Template admin facade methods

--- vouformar/app/controllers/admin_facade_controller.rb
+++ vouformar/app/controllers/admin_facade_controller.rb
@@ -1,2 +1,22 @@
 class AdminFacadeController < ApplicationController
+  def disciplines_index
+  end
+
+  def disciplines_show
+  end
+
+  def disciplines_new
+  end
+
+  def disciplines_edit
+  end
+
+  def disciplines_create
+  end
+
+  def disciplines_update
+  end
+
+  def disciplines_destroy
+  end
 end
